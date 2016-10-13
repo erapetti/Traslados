8f122eb9fe7d21c77aec21ed9bba68156dde3570
Ernesto Rapetti <erapetti@yahoo.com>
2016-10-13 14:50:52 -0300

    Ajuste para ignorar espacios en la COOKIE de sesión

--- api/controllers/ProcesoController.js
+++ api/controllers/ProcesoController.js
@@ -11,7 +11,7 @@ module.exports = {
 		if (sails.config.environment === "development") {
 			sessionid = '9728448076454730240';
 		} else {
-			sessionid = req.cookies.SESION;
+			sessionid = req.cookies.SESION.replace(/[+ ]/g,'');
 		}
 		wsPortal.getSession(sessionid, function(err,session) {
 			if (sails.config.environment === "development") {
