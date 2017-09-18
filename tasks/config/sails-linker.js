0b689cdec1b20fcdc74a1b4f7b665471f892b032
Ernesto Rapetti <erapetti@yahoo.com>
2017-09-18 12:15:20 -0300

    Agrego número de versión a los archivos

--- tasks/config/sails-linker.js
+++ tasks/config/sails-linker.js
@@ -63,7 +63,7 @@ module.exports = function(grunt) {
       options: {
         startTag: '<!--SCRIPTS-->',
         endTag: '<!--SCRIPTS END-->',
-        fileTmpl: '<script src="/node/traslados%s"></script>',
+        fileTmpl: '<script src="/node/traslados%s?v=<%- (new Date()).getTime() %>"></script>',
         appRoot: '.tmp/public'
       },
       files: {
@@ -123,7 +123,7 @@ module.exports = function(grunt) {
       options: {
         startTag: '<!--STYLES-->',
         endTag: '<!--STYLES END-->',
-        fileTmpl: '<link rel="stylesheet" href="/node/traslados%s">',
+        fileTmpl: '<link rel="stylesheet" href="/node/traslados%s?v=<%- (new Date()).getTime() %>">',
         appRoot: '.tmp/public'
       },
       files: {
